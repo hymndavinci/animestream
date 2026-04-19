0b9c52693bf13e2538f770ad7fb40204461a34b1
hymndavinci <era90tahunan@gmail.com>
2026-04-19 18:56:39 +0700

    fix: add CSP headers for iframe embed

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,34 @@ const nextConfig = {
       { protocol: "https", hostname: "**" },
       { protocol: "http", hostname: "**" }
     ]
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src 'self' data: blob: https: http:",
+              "font-src 'self' data: https:",
+              "connect-src 'self' https: http:",
+              "media-src 'self' https: http: blob:",
+              "frame-src https://2embed.cc https://*.2embed.cc https://www.miruro.tv https://*.miruro.tv https://vidsrc.to https://*.vidsrc.to https://vidsrc.me https://*.vidsrc.me https://vidsrc.xyz https://*.vidsrc.xyz https://embed.su https://*.embed.su",
+              "frame-ancestors *",
+            ].join("; "),
+          },
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
