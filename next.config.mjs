805caf9133e24395987564b00e3798f9342f495d
hymndavinci <era90tahunan@gmail.com>
2026-04-19 19:26:15 +0700

    fix: add vsembed.ru and vidsrcme.ru to CSP

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ const nextConfig = {
               "font-src 'self' data: https:",
               "connect-src 'self' https: http:",
               "media-src 'self' https: http: blob:",
-              "frame-src https://2embed.cc https://*.2embed.cc https://www.miruro.tv https://*.miruro.tv https://vidsrc.to https://*.vidsrc.to https://vidsrc.me https://*.vidsrc.me https://vidsrc.xyz https://*.vidsrc.xyz https://embed.su https://*.embed.su",
+              "frame-src https://2embed.cc https://*.2embed.cc https://www.miruro.tv https://*.miruro.tv https://vidsrc.to https://*.vidsrc.to https://vidsrc.me https://*.vidsrc.me https://vidsrc.xyz https://*.vidsrc.xyz https://embed.su https://*.embed.su https://vidsrcme.ru https://*.vidsrcme.ru https://vsembed.ru https://*.vsembed.ru",
               "frame-ancestors *",
             ].join("; "),
           },
